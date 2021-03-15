7b2c09e20516c9d18163e550c021bdc68ad2f282
Dev-Beom <66074802+Dev-Beom@users.noreply.github.com>
2021-03-15 18:06:50 +0900

    Add Api

---
+++ src/main/java/com/spring_study/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.spring_study.springboot.service.posts;
+
+import com.spring_study.springboot.domain.posts.PostsRepository;
+import com.spring_study.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
