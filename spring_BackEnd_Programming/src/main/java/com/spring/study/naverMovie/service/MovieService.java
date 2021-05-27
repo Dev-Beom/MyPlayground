feab7496114d6aa4f713d4764b08326892c5f876
Dev-Beom <youngbeom0908@gmail.com>
2021-05-28 00:11:31 +0900

    Update 영화 검색 서비스 평점 순 정렬 기능 추가

--- spring_BackEnd_Programming/src/main/java/com/spring/study/naverMovie/service/MovieService.java
+++ spring_BackEnd_Programming/src/main/java/com/spring/study/naverMovie/service/MovieService.java
@@ -1,5 +1,6 @@
 package com.spring.study.naverMovie.service;
 import com.spring.study.naverMovie.domain.Movie;
+import com.spring.study.naverMovie.domain.MovieGroup;
 import com.spring.study.naverMovie.domain.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ public class MovieService {
     private final MovieRepository movieRepository;
 
     public List<Movie> search(final String query) {
-        return movieRepository.findByQuery(query);
+        MovieGroup movieGroup = new MovieGroup(movieRepository.findByQuery(query));
+        return movieGroup.getListOrderRating();
     }
 }
