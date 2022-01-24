ae8d54de5e0c33d5426aaf63141bdaef72619456
youngbeom0908 <youngbeom0908@gmail.com>
2022-01-24 18:17:56 +0900

    Refactor: 4장 기본 코드 추가

--- spring5fs-chap03/src/main/java/main/MainForSpring.java
+++ spring5fs-chap03/src/main/java/main/MainForSpring.java
@@ -1,6 +1,5 @@
 package main;
 
-import assembler.Assembler;
 import config.AppCtx;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
